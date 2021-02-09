3473f277dcc66e7e52149c87cf740f04aa105a7c
zvnico <nicolas.baconnier@efrei.net>
2021-02-09 14:04:20 +0100

    Main gen

--- src/TD1/Main.java
+++ src/TD1/Main.java
@@ -4,7 +4,7 @@ import TD2.Segment;
 
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(1,3);
+        Point point = new Point(1, 3);
         point.print();
     }
 }
--- src/TD2/Segment.java
+++ src/TD2/Segment.java
@@ -5,12 +5,10 @@ import TD1.Point;
 public class Segment {
     private Point point1;
     private Point point2;
-
     public Segment(int x1, int y1, int x2, int y2) {
         point1 = new Point(x1, y1);
         point2 = new Point(x2, y2);
     }
-
     public void print() {
         System.out.println(toString());
     }
